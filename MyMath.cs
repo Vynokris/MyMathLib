8a57d53652bdbf54f8d40e72c602afc216f6685d
Alexandre PERCHE <a.perche@student.isartdigital.com>
2022-03-08 09:42:56 +0100

    First commit for CSharp branch.

---
+++ MyMath.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyMathLib
+{
+    public static class Arithmetic
+    {
+        public static int RoundInt(ref float val)  { return (int)Math.Round(val);  }
+        public static int FloorInt(ref float val)  { return (int)Math.Floor(val);  }
+        public static int CeilInt(ref float val)   { return (int)Math.Ceiling(val); }
+        public static float SqPow(ref float val)   { return val * val;  }
+        public static int SignOf(ref float val)    { if (val == 0) return 1; return (int)val / Math.Abs((int)val); }
+        public static float Deg2Rad(ref float val) { return val * ((float)Math.PI / 180f); }
+    }
+}
--- README.md
+++
@@ -1,4 +0,0 @@
-# My Math Lib project
-
-This project is a partial recreation of common math libraries done in C.<br>
-All the code of the repository is personal and I will use this library to make games in the future.
